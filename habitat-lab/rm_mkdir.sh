3e4309bab14e79a59707d1059641784be6d74d00
joannetruong <truong.j@gatech.edu>
2022-03-09 16:37:54 -0500

    migrate multi_nav code

--- habitat-lab/rm_mkdir.sh
+++
@@ -1,7 +0,0 @@
-DIR=$1
-
-rm -rf /coc/pskynet3/jtruong33/develop/flash_results/habitat_spot_results/${DIR}
-mkdir -p /coc/pskynet3/jtruong33/develop/flash_results/habitat_spot_results/${DIR}/video_dir
-mkdir -p /coc/pskynet3/jtruong33/develop/flash_results/habitat_spot_results/${DIR}/tb
-mkdir -p /coc/pskynet3/jtruong33/develop/flash_results/habitat_spot_results/${DIR}/checkpoints
-mkdir -p /coc/pskynet3/jtruong33/develop/flash_results/habitat_spot_results/${DIR}/txts
