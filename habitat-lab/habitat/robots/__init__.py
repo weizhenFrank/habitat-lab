1053ff42c5dbff8ca3f8573188c2e285b7b30909
joannetruong <truong.j@gatech.edu>
2022-11-11 18:43:34 -0500

    remove rearrange robots, add quadrupeds

--- habitat-lab/habitat/robots/__init__.py
+++ habitat-lab/habitat/robots/__init__.py
@@ -4,27 +4,46 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from habitat.robots.fetch_robot import FetchRobot, FetchRobotNoWheels
-from habitat.robots.franka_robot import FrankaRobot
-from habitat.robots.mobile_manipulator import (
-    MobileManipulator,
-    MobileManipulatorParams,
-    RobotCameraParams,
+# from habitat.robots.fetch_robot import FetchRobot, FetchRobotNoWheels
+# from habitat.robots.franka_robot import FrankaRobot
+# from habitat.robots.mobile_manipulator import (
+#     MobileManipulator,
+#     MobileManipulatorParams,
+#     RobotCameraParams,
+# )
+# from habitat.robots.robot_interface import RobotInterface
+# from habitat.robots.static_manipulator import (
+#     StaticManipulator,
+#     StaticManipulatorParams,
+# )
+#
+# __all__ = [
+#     "RobotInterface",
+#     "MobileManipulatorParams",
+#     "MobileManipulator",
+#     "FetchRobot",
+#     "FetchRobotNoWheels",
+#     "RobotCameraParams",
+#     "StaticManipulatorParams",
+#     "StaticManipulator",
+#     "FrankaRobot",
+# ]
+
+from habitat.robots.daisy_env import Daisy, Daisy_4legged
+from habitat.robots.raibert_controller import (
+    Raibert_controller_turn,
+    Raibert_controller_turn_stable,
 )
-from habitat.robots.robot_interface import RobotInterface
-from habitat.robots.static_manipulator import (
-    StaticManipulator,
-    StaticManipulatorParams,
-)
+from habitat.robots.robot_env import A1, AlienGo, Laikago, Locobot, Spot
 
 __all__ = [
-    "RobotInterface",
-    "MobileManipulatorParams",
-    "MobileManipulator",
-    "FetchRobot",
-    "FetchRobotNoWheels",
-    "RobotCameraParams",
-    "StaticManipulatorParams",
-    "StaticManipulator",
-    "FrankaRobot",
+    "Daisy",
+    "Daisy_4legged",
+    "Raibert_controller_turn",
+    "Raibert_controller_turn_stable",
+    "A1",
+    "AlienGo",
+    "Laikago",
+    "Locobot",
+    "Spot",
 ]
