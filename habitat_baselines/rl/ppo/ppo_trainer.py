4a89a9cfc27f1af65ab5efe952186291dcecc83c
weizhenFrank <frankliu1998@icloud.com>
2023-10-16 16:19:38 -0400

    change the TensorDict to always sort on alphabetical order

--- habitat_baselines/rl/ppo/ppo_trainer.py
+++ habitat_baselines/rl/ppo/ppo_trainer.py
@@ -332,6 +332,7 @@ class PPOTrainer(BaseRLTrainer):
         if self._static_encoder:
             with torch.no_grad():
                 batch["visual_features"] = self._encoder(batch)
+        import pdb; pdb.set_trace()
         self.rollouts.buffers["observations"][0] = batch
 
         self.current_episode_reward = torch.zeros(self.envs.num_envs, 1)
